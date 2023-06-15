88742927deeacea8913194b0d4a252aa3c0f2e2d
Michael Brett <mdouglasbrett@icloud.com>
2023-06-15 18:24:23 +0100

    Adds constructor

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -18,9 +18,14 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        // TODO: using clone would probably not be idiomatic in a larger program,
+        // and may be revisited in a later exercise in the book.
+        // I suspect _lifetimes_ 🙀
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config { query, file_path }
+        Config { query, file_path }
+    }
 }
