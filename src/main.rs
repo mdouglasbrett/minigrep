3bd508545c4d5d8ecd05eb0de4ca404fba114139
Michael Brett <mdouglasbrett@icloud.com>
2023-06-16 19:57:19 +0100

    Uses standard error for error output

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use std::{env, process};
 use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-
     // We only care about the Err here, so unwrap_or_else is uneeded
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
